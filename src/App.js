47529c51871a4630f34f6dbe5b203a43a4038162
vlmnst <vale-mansueto@hotmail.com>
2022-08-29 15:53:03 -0300

    message

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from 'react'
+import Table from "./components/Table";
+import Header from "./components/Header";
+import Form from "./components/Form";
+import { Route, Routes } from "react-router-dom";
+import data from './data.json'
+import './App.css'
 function App() {
+  
+  const [info, setInfo] = useState(data)
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app" >
+      <Header/>
+      <Routes>
+        <Route  path={"/"} element={<Table info={info} />} />
+        <Route  path={"/formulario"} element={<Form setInfo={setInfo}  info={info}/>} />
+      </Routes>
+
+      
     </div>
   );
 }
